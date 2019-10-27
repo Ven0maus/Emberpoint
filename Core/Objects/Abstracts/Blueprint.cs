af20f14c5196d2e8a8cfbc3c6be09beea427db0c
Venomous <stefvandenbruel@outlook.com>
2019-10-27 16:59:14 +0100

    Issue #4: House design and blueprint in txt format

---
+++ Core/Objects/Abstracts/Blueprint.cs
@@ -0,0 +1,13 @@
+﻿using SadConsole;
+using System;
+
+namespace Emberpoint.Core.Objects.Abstracts
+{
+    public class Blueprint<T> where T : Cell
+    {
+        public virtual T[] GetCells()
+        {
+            return Array.Empty<T>();
+        }
+    }
+}
---
+++ Core/Objects/Blueprints/HouseBlueprint.cs
@@ -0,0 +1,24 @@
+﻿using Emberpoint.Core.Objects.Abstracts;
+using Microsoft.Xna.Framework;
+
+namespace Emberpoint.Core.Objects.Blueprints
+{
+    public class HouseBlueprint : Blueprint<EmberCell>
+    {
+        public override EmberCell[] GetCells()
+        {
+            // TODO: Read from .txt file and .json config file
+            int gridSizeX = 70;
+            int gridSizeY = 30;
+            var cells = new EmberCell[gridSizeX * gridSizeY];
+            for (int x = 0; x < gridSizeX; x++)
+            {
+                for (int y = 0; y < gridSizeY; y++)
+                {
+                    cells[y * gridSizeX + x] = new EmberCell(new Point(x, y), '.', Color.Green);
+                }
+            }
+            return cells;
+        }
+    }
+}
