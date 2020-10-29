e78e5c87e797e5b3413fec726c7e60a2a0f35800
Agit <agitbasar@yahoo.be>
2020-10-29 15:08:04 +0100

    issue64-ab

--- Core/GameObjects/Abstracts/Blueprint.cs
+++ Core/GameObjects/Abstracts/Blueprint.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -129,6 +130,27 @@ namespace Emberpoint.Core.GameObjects.Abstracts
 
         private Color GetColorByString(string value)
         {
+            if (value.StartsWith("#"))
+            {
+                value = value.TrimStart('#');
+                Color color;
+
+                if (value.Length == 6)
+                    color = new Color(
+                                int.Parse(value.Substring(0, 2), NumberStyles.HexNumber),
+                                int.Parse(value.Substring(2, 2), NumberStyles.HexNumber),
+                                int.Parse(value.Substring(4, 2), NumberStyles.HexNumber),
+                                255);
+                else // assuming length of 8
+                    color = new Color(
+                                int.Parse(value.Substring(2, 2), NumberStyles.HexNumber),
+                                int.Parse(value.Substring(4, 2), NumberStyles.HexNumber),
+                                int.Parse(value.Substring(6, 2), NumberStyles.HexNumber),
+                                int.Parse(value.Substring(0, 2), NumberStyles.HexNumber));
+
+                return color;
+            }
+
             var prop = typeof(Color).GetProperty(value);
             if (prop != null)
                 return (Color)prop.GetValue(null, null);
