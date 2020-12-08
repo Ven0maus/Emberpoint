3e0f62cb02951c082bd8262fb09cd0644015d29e
Venomous <stefvandenbruel@outlook.com>
2020-12-08 17:40:26 +0100

    fix

--- Core/GameObjects/Managers/KeybindingsManager.cs
+++ Core/GameObjects/Managers/KeybindingsManager.cs
@@ -17,7 +17,7 @@ namespace Emberpoint.Core.GameObjects.Managers
                 (Keybindings.Movement_Left, Keys.Left),
                 (Keybindings.Movement_Right, Keys.Right),
                 (Keybindings.Flashlight, Keys.F),
-                (Keybindings.Interact, Keys.G)
+                (Keybindings.Interact, Keys.G),
                 (Keybindings.DeveloperConsole, Keys.A) 
             };
 
@@ -52,7 +52,7 @@ namespace Emberpoint.Core.GameObjects.Managers
         Movement_Left,
         Movement_Right,
         Flashlight,
-        Interact
+        Interact,
         DeveloperConsole
     }
 }
